d4a3d507a5e6f7ce1246bc9745719a8298b0678e
lovezah <zah0109@126.com>
2022-05-29 17:53:51 +0800

    update RMQ.cpp

--- template/RMQ.cpp
+++ template/RMQ.cpp
@@ -1,44 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-template<typename T> struct RMQ {
+template<typename T, bool maximum_mode = false>
+struct RMQ {
 	inline int highest_bit(unsigned x) {
 		return x == 0 ? -1 : 31 - __builtin_clz(x);
 	}
 
-	int n;
-	vector<vector<T>> f;
+	int n = 0;
+	vector<T> values;
+	vector<vector<int>> range;
 	RMQ(const vector<T> &v = {}) {
-		if(!v.empty()) {
+		if(!v.empty())
 			build(v);
-		}
 	}
-	
-	T better(T a, T b) {
-		return max(a, b);
+
+	// when values[a] == values[b], return b
+	int better_index(int a, int b) const {
+		return (maximum_mode ? values[b] < values[a] : values[a] < values[b]) ? a : b;
 	}
 
 	void build(const vector<T> &v) {
-		n = int(v.size());
+		values = v;
+		n = int(values.size());
 		int levels = highest_bit(n) + 1;
-		f.resize(levels);
+		range.resize(levels);
 
-		for(int k = 0; k < levels; k++) {
-			f[k].resize(n - (1 << k) + 1);
-		}
-
-		if(n > 0) f[0] = v;
-
-		for(int k = 1; k < levels; k++) {
-			for(int i = 0; i <= n - (1 << k); i++) {
-				f[k][i] = better(f[k - 1][i], f[k - 1][i + (1 << (k - 1))]);
-			}
-		}
+		for(int k = 0; k < levels; k++)
+			range[k].resize(n - (1 << k) + 1);
+		for(int i = 0; i < n; i++)
+			range[0][i] = i;
+		for(int k = 1; k < levels; k++)
+			for(int i = 0; i <= n - (1 << k); i++)
+				range[k][i] = better_index(range[k - 1][i], range[k - 1][i + (1 << (k - 1))]);
 	}
 
-	T query(int a, int b) {
+	int query_index(int a, int b) {
+		assert(a >= 0 && a < b && b <= n);
 		int level = highest_bit(b - a);
-		return better(f[level][a], f[level][b - (1 << level)]);
+		return better_index(range[level][a], range[level][b - (1 << level)]);
+	}
+
+	T query_value(int a, int b) {
+		return values[query_index(a, b)];
 	}
 };
 
