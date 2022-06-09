ad8381c21db08594a1b44734e6401a47d6d78f9c
lovezah <zah0109@126.com>
2022-06-09 18:45:38 +0800

    update temp

--- template/MulCaseTemplate.cpp
+++ template/MulCaseTemplate.cpp
@@ -17,25 +17,6 @@ template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator
 	#define dbg(x...)
 #endif
 
-#define MAX(v) max_element((v).begin(), (v).end())
-#define MIN(v) min_element((v).begin(), (v).end())
-#define ALL(v) (v).begin(), (v).end()
-#define RALL(v) (v).rbegin(), (v).rend()
-#define SUM(v, x) accumulate(ALL(v), (x))
-#define UNIQUE(v) (v).erase(unique(ALL(v)), (v).end())
-void YES(bool t = true) { cout << (t ? "YES" : "NO") << "\n"; }
-void Yes(bool t = true) { cout << (t ? "Yes" : "No") << "\n"; }
-void yes(bool t = true) { cout << (t ? "yes" : "no") << "\n"; }
-void neg() { cout << "-1" << "\n"; }
-template<typename T> bool ckmin(T &a, T b) { return b < a ? a = b, true : false; }
-template<typename T> bool ckmax(T &a, T b) { return b > a ? a = b, true : false; }
-template<class T> using pq = priority_queue<T>;
-template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
-template<typename T_vector = int> 
-vector<vector<T_vector>> vv(int row, int col, T_vector details = 0) { 
-	return vector<vector<T_vector>> (row, vector<T_vector> (col, details)); 
-}
-
 void run_case() {
 
 }
