f5d675c3380ec43c3c3aeccc51744e932628c96c
lovezah <zah0109@126.com>
2022-06-11 14:39:25 +0800

    update

--- template/LazyCaseTemplate.cpp
+++ template/LazyCaseTemplate.cpp
@@ -18,23 +18,25 @@ template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator
 #endif
 
 #define rep(i, n) for(int i = 0; i < (n); i++)
-#define REP(i, a, n) for(int i = (a); i < (n); i++)
+#define repp(i, a, n) for(int i = (a); i < (n); i++)
 #define per(i, n) for(int i = (n) - 1; i >= 0; i--)
-#define PER(i, a, n) for(int i = (n) - 1; i >= (a); i--)
+#define perr(i, a, n) for(int i = (n) - 1; i >= (a); i--)
 #define trav(a, v) for(auto &(a) : (v))
 #define pb push_back
 #define eb emplace_back
 #define mp make_pair
 #define ALL(x) (x).begin(), (x).end()
-#define SZ(x) int((x).size())
+#define SZ(x) (int((x).size()))
 #define MAX(x) max_element(ALL(x))
 #define MIN(x) min_element(ALL(x))
 #define SUM(x, v) accumulate(all(X), (v))
 #define UNIQUE(x) x.erase(unique(ALL(x)), x.end())
+#define SORT_UNIQUE(x) (sort(ALL(x)), x.erase(unique(ALL(x)), x.end()))
 #define fi first
 #define se second
 using i64 = int64_t;
 using VI = vector<int>;
+using VB = vector<bool>;
 using V64 = vector<i64>;
 using PII = pair<int, int>;
 template<class T> bool ckmin(T &a, T b) { return b < a ? a = b, true : false; }
@@ -43,6 +45,16 @@ template<typename T_vector>
 vector<vector<T_vector>> VV(int row, int col, int details) {
 	return vector<vector<T_vector>> (row, vector<T_vector> (col, details));
 }
+template<class T> void _R(T &x) { cin >> x; }
+template<class T, class U> void _R(pair<T, U> &x) { cin >> x.fi >> x.se; }
+void R() {}
+template<class T, class... U> void R(T &head, U &... tail) { _R(head); R(tail...); }
+template<class T> void _W(const T &x) { cout << x; }
+void _W(const double &x) { cout << fixed << setprecision(20) << x; }
+template<class T, class U> void _W(const pair<T, U> &x) { _W(x.fi); cout << " "; _W(x.se); }
+template<class T> void _W(const vector<T> &x) { for(auto i = x.begin(); i != x.end(); _W(*i++)) if(i != x.cbegin()) cout << " "; }
+void W() {}
+template<class T, class... U> void W(const T &head, const U &... tail) { _W(head); cout << (sizeof...(tail) ? " " : "\n"); W(tail...); }
 inline void YES(bool t = true) { cout << (t ? "YES" : "NO") << "\n"; }
 inline void Yes(bool t = true) { cout << (t ? "Yes" : "No") << "\n"; }
 inline void yes(bool t = true) { cout << (t ? "yes" : "no") << "\n"; }
@@ -58,10 +70,9 @@ int main () {
 	cin.tie(nullptr);
 #endif
 
-	cout << setprecision(20);
 
 	int tc = 1; 
-	cin >> tc; 
+	cin >> tc;
 	while(tc--) {
 		run_case();
 	}
