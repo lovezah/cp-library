27ef01ae13c661da7d7943bc8c784e464cdf671e
lovezah <zah0109@126.com>
2022-08-08 14:16:36 +0800

    update

--- template/MulCaseTemplate.cpp
+++ template/MulCaseTemplate.cpp
@@ -1,14 +1,22 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include<iomanip>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <bitset>
+#include <array>
 using namespace std;
-
-// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
-template<class Fun> class y_combinator_result {
-    Fun fun_;
-public:
-    template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {}
-    template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); }
-};
-template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
 
 #ifdef DEBUG
 	#include "debug.h"
@@ -17,7 +25,23 @@ template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator
 	#define dbg(x...)
 #endif
 
-using i64 = int64_t;
+using ll = long long;
+using uint = unsigned int;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<class T> bool ckmin(T &a, T b) {return a > b ? a = b, true : false;}
+template<class T> bool ckmax(T &a, T b) {return a < b ? a = b, true : false;}
 
 void run_case() {
 	
