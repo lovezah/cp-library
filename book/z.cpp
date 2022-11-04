8c0386a3d31850710e0c1cf0ad4bc5c2498fef7f
lovezah <88532066+lovezah@users.noreply.github.com>
2022-11-04 20:15:43 +0800

    Update z.cpp

--- book/z.cpp
+++ book/z.cpp
@@ -1,10 +1,13 @@
-vector<int> get_z(string s) {
+#include <bits/stdc++.h>
+
+std::vector<int> get_z(std::string s) {
     int n = int(s.size());
-    vector<int> z(n);
+	std::vector<int> z(n);
     for (int i = 1, l = 0, r = 0; i < n; i++) {
-        if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+        if (i <= r) z[i] = std::min(r - i + 1, z[i-l]);
         while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i] += 1;
         if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
     }
     return z;
 }
+
