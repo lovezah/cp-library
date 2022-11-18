b1ffd6c77e0dca577701e1a12cf846c7e721ba61
lovezah <zah0109@126.com>
2022-11-18 08:31:45 +0800

    change gen.cpp to random.cpp

---
+++ book/random.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+int main() {
+	using namespace std;
+	
+}
