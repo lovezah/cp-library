311b1454c21870e58722f7add3005e777f9e3f01
lovezah <zah0109@126.com>
2022-12-10 22:36:25 +0800

    update DSU

--- dsu.cpp
+++ dsu.cpp
@@ -1,39 +1,25 @@
+#include <bits/stdc++.h>
 
-struct dsu {
-	vector<int> par, size;
-	int tot = 0;
+struct DSU {
+	std::vector<int> par;
+	int n, component;
 
-	dsu(int n) {
-		par.resize(n + 1);
-		size.assign(n + 1, 1);
-		tot = n;
-
-		for(int i = 0; i <= n; i++)
-			par[i] = i;
+	DSU(int n_) : n(n_), component(n_), par(n_, -1) {}
+	int get_par(int a) {
+		while (par[a] >= 0) {
+			if (par[par[a]] >= 0) par[a] = par[par[a]];
+			a = par[a];
+		}
+		return a;
 	}
-
-	int find(int x) {
-		return x == par[x] ? x : par[x] = find(par[x]);
+	bool unite(int a, int b) {
+		a = get_par(a), b = get_par(b);
+		if (a == b) return false;
+		if (par[a] > par[b]) std::swap(a, b);
+		par[a] += par[b];
+		par[b] = a;
+		return true;
 	}
-
-	void merge(int x, int y) {
-		x = find(x);
-		y = find(y);
-
-		if(x > y) 
-			swap(x, y);
-
-		par[y] = x;
-		sz[x] += sz[y];
-		tot--;
-	}
-
-    bool same(int x, int y) {
-        return find(x) == find(y);
-    }
-
-	int sz(int x) {
-		return size[find(x)];
-	}
+	bool same(int a, int b) { return get_par(a) == get_par(b); }
+	int size(int a) { return -par[get_par(a)]; }
 };
-
